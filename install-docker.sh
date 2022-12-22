c25257b61de9794873ff479cd0516a683066e572
Randy Bartels <rjbartels@outlook.com>
2022-12-22 09:23:39 -0500

    WSL logic and color
    Signed-off-by: Randy Bartels <rjbartels@outlook.com>

--- install-docker.sh
+++ install-docker.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+kernelRelease="$(uname -r)"
+prettyName="$(grep PRETTY_NAME /etc/os-release)"
+FORE_RED='\033[31m'
+DEFAULT_COLOR='\033[m'
+
 # Update our APT databases and upgrade any out of date packages
-sudo apt update && sudo apt-get upgrade
+sudo apt update && sudo apt-get --assume-yes upgrade
 
 # Make sure some necessary dependencies are installed
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install --assume-yes apt-transport-https ca-certificates curl software-properties-common
 
 # Fetch the Docker GPG key so we can validate that the packages we're about to install haven't been tampered with
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -17,14 +22,25 @@ sudo apt update
 
 # Confirm that we're getting Docker-CE from Docker and not from any other source
 apt-cache policy docker-ce
-echo "Review the information above and make sure the download URLs show 'https://download.docker.com'.  Press ENTER to continue or CTRL-C to abort installation"
+printf "Review the information above and make sure the ${FORE_RED}download URLs show 'https://download.docker.com'${DEFAULT_COLOR}.  Press ENTER to continue or CTRL-C to abort installation\n"
 read dummyVar
 
 # Install Docker already
-sudo apt install docker-ce
+sudo apt install --assume-yes docker-ce
 
 # Update group permissions so the current user can run Docker commands without SUDO
 sudo usermod -aG docker ${USER}
 
-echo "Installation is complete"
-echo "Group permissions modified.  You will need to logout before group changes will take effect."
+if [[ $kernelRelease =~ "WSL" ]]; then
+  if [[ $prettyName =~ "Ubuntu 22" ]]; then
+    printf "\n\nUbuntu 22.04 detected on WSL.  We need to reset IPTables to use the legacy engine.\n"
+    printf "${FORE_RED}Select 'iptables-legacy' (usually #1) on the following prompt${DEFAULT_COLOR}\n\n"
+    sudo update-alternatives --config iptables
+  fi
+  printf "\nWSL installation detected\n"
+  printf "You'll need to start Docker each time WSL reboots.  Use the following command:\n"
+  printf "\t${FORE_RED}sudo service docker start${DEFAULT_COLOR}\n"
+fi
+
+printf "Installation is complete\n"
+printf "Docker group permissions modified.\n${FORE_RED}You will need to logout before group changes will take effect.${DEFAULT_COLOR}\n"
